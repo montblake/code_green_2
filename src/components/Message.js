468e1cf9bc03ce541817a8757cc088cd74502c27
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-07-20 15:26:27 -0400

    CSS styling enhancements

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,11 +4,12 @@ function Message(props) {
     return (
         <div className="message">
             
-            <h4>{props.user}</h4>
-            {/* <p>{moment(props.created_at).format('MMMM Do YYYY')}</p>
-            <p>{moment(props.created_at).fromNow()}</p> */}
-            <p>{moment(props.created_at).format('LT')}</p>
-            <p>{moment(props.created_at).format('LL')}</p>
+            <h4>{props.user} 
+            <span class="messagetime">{moment(props.created_at).format('LT')} 
+            &middot; {moment(props.created_at).format('LL')}</span>
+            </h4>
+           
+            <p></p>
             <h3>{props.content}</h3>
         </div>
     )
