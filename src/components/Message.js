3e902751f328d5528b272b07a720ca74c7d37f8f
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-07-22 13:33:07 -0400

    class to ClassName corrections

--- src/components/Message.js
+++ src/components/Message.js
@@ -9,16 +9,16 @@ function Message(props) {
     return (
         <div className="message">
 
-            <div class="messageLeftside">
+            <div className="messageLeftside">
                 <h4>{props.user}
                     <span className="messagetime">{moment(props.created_at).format('LT')}
                         &middot; {moment(props.created_at).format('LL')}</span>
 
-                    <button onClick={handleDelete} class="crudbutton deleteButton">
-                        <i class="fa fa-trash-o fa-2x" aria-hidden="true"> </i>
+                    <button onClick={handleDelete} className="crudbutton deleteButton">
+                        <i className="fa fa-trash-o fa-2x" aria-hidden="true"> </i>
                     </button>
-                    <button class="crudbutton editButton">
-                        <i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i>
+                    <button className="crudbutton editButton">
+                        <i className="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i>
                     </button>
 
                 </h4>
